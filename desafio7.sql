06d9050967dd61aee24a7faf1007bb81c55ff42f
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-03-10 13:39:48 -0300

    feat: task 7 complete

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT
+	arts.name AS `artista`,
+    a.name AS `albuns`,
+    COUNT(f.user_id) AS `seguidores`
+FROM SpotifyClone.artists AS arts
+INNER JOIN SpotifyClone.albuns AS a
+	ON a.artist_id = arts.id
+INNER JOIN SpotifyClone.followingArtists AS f
+	ON f.artist_id = arts.id
+GROUP BY arts.name, a.name
+ORDER BY `seguidores` DESC, arts.name ASC, a.name ASC;
